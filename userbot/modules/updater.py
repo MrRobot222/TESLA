e84990410a52137c82a358fb4814a7245213ef0d
MrRobot222 <jokerhacker.6521@protonmail.com>
2021-03-12 21:13:23 +0530

    minor bug fixes

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -85,7 +85,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         else:
             remote = repo.create_remote("heroku", heroku_git_url)
         try:
-            remote.push(refspec="HEAD:refs/heads/master", force=True)
+            remote.push(refspec="HEAD:refs/heads/devil", force=True)
         except GitCommandError as error:
             await event.edit(f'{txt}\n`Here is the error log:\n{error}`')
             return repo.__del__()
@@ -151,7 +151,7 @@ async def upstream(event):
         origin = repo.create_remote('upstream', off_repo)
         origin.fetch()
         force_update = True
-        repo.create_head('master', origin.refs.master)
+        repo.create_head('demon', origin.refs.master)
         repo.heads.master.set_tracking_branch(origin.refs.master)
         repo.heads.master.checkout(True)
 
