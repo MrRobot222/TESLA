75b8a17249ab7ccd02a3b3a5b0d37594b1f02cfc
MrRobot222 <jokerhacker.6521@protonmail.com>
2021-03-25 13:11:38 +0530

    Working with .nmap module

--- userbot/modules/nmap.py
+++ userbot/modules/nmap.py
@@ -1,12 +1,17 @@
-import nmap3
+from typing import Pattern
+import nmap
 from userbot import CMD_HELP
 from userbot.events import register
 
 @register(outgoing=True, pattern="^.nmap (.*)")
 async def nmap(event):
     await event.edit("`Processing...`")
+    nm = nmap.PortScanner()
     ip = event.pattern_match.group(1)
-    print(ip)
+    port = event.pattern_match.group(2)
+    result = nm.scan(ip, port)
+
+    await event.edit(result)
    
 
 
