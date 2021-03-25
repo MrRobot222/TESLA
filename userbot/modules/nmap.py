86fc7f1a46f4aa0987d3b805829b79b594926627
MrRobot222 <jokerhacker.6521@protonmail.com>
2021-03-25 17:13:31 +0530

    working with .nmap module

--- userbot/modules/nmap.py
+++ userbot/modules/nmap.py
@@ -8,7 +8,6 @@ from userbot.events import register
 async def nmap_fun(event):
     await event.edit("`Processing...`")
     nm = nmap.PortScanner()
-    os.system("apt-get install nmap")
     ip = event.pattern_match.group(1)
     port = event.pattern_match.group(2)
     result = nm.scan(ip, port)
